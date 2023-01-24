6890ba8c51dc3990b3bf43ed21e63c0015bea846
Karan Singh <122407163+sing5998@users.noreply.github.com>
2023-01-24 16:49:25 -0500

    Update StudentList.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -11,6 +11,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * list to print each name.
  * @author dancye, Jan 2019
  */
+//I made this change Online
 public class StudentList {
     
     public static void main(String[] args)
